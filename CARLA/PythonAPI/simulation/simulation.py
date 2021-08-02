61f1fbd4e5fafbc6412a0b40e0748e570035b37a
Ethvedbitdesjan <ved6734@gmail.com>
2021-08-03 03:56:20 +0530

    added recorder functions

--- CARLA/PythonAPI/simulation/simulation.py
+++ CARLA/PythonAPI/simulation/simulation.py
@@ -1,5 +1,5 @@
 from helper import helper
-
+import os
 class simulation():
     def __init__(self, client=None):
         if client is None:
@@ -18,6 +18,34 @@ class simulation():
     def reset_simulation(self):
         helper.reset_carla_world(self.client)
 
-    # record a scene 
+    def record_scene(self, filename="recording01.log", additional_param=True):
+        self.client.start_recorder(filename, additional_param)
 
-    # replay a scene 
+    def stop_record(self):
+        self.client.stop_recorder()
+
+    def replay_scene(self, filename="recording01.log", start=1, duration=0, camera=0, time_factor=1.0):
+        self.set_recorder_playback(time_factor)
+        self.client.replay_file(filename, start, duration, camera)
+
+    def set_recorder_playback(self, time_factor):
+        self.client.set_replayer_time_factor(time_factor)
+
+    def store_recorded_data(self, filename="recording01.log", show_all= True, txt_file="output.txt"):
+
+        output = self.client.show_recorder_file_info(filename, show_all)
+
+        # creates the text file if it does not exist
+        file = open(txt_file, "w+")
+        file.close()
+
+        # deletes all previous content from text file
+        file = open(txt_file, "r+")
+        file.truncate(0)
+        file.close()
+
+        # writes recorder output to text file
+        text_file = open(txt_file, "w")
+        n = text_file.write(output)
+        text_file.close()
+    
